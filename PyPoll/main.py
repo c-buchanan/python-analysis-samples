467726a434f26c90704b367da58a9780d10e46de
c-buchanan <courtney.p.buchanan@gmail.com>
2018-10-16 14:29:54 -0400

    A sample of basic analytical work using Python.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,28 @@
-# This is the start of my assignment 
+# Import the dataset by reading the CSV file 
+import os
+import csv
+
+# Store List of Values
+votes = []
+
+
+PyPoll_CSV_Path = os.path.join('Resources', 'election_data.csv')
+with open(PyPoll_CSV_Path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next (csvreader)     
+
+    for row in csvreader:
+        votes.append(row[0])
+        total_votes = set(votes) 
+        
+
+
+
+
+
+# Print Election Results 
+print('---------------------------------')
+print('Election Analysis')
+print('---------------------------------')
+print('Total Votes: ' + str(len(total_votes)))
+
